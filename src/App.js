1a409aa6f7b60672c22a9c25d153401c4ef20efa
balasaravananj <balasaravanan2000@gmail.com>
2023-11-22 15:54:37 +0530

    added useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
 
 function App() {
+
+  const [userQuery, setUserQuery] = useState('');
+
+  const searchQuery = () =>{
+    window.open(`https://google.com/search?q=${userQuery}`,'_blank')
+  }
+
+ const updateUserQuery = (event) => {
+  console.log('userQuery', userQuery);
+   setUserQuery(event.target.value)
+  }
+
+  const keyPressHandler = (event) => {
+    if(event.key === 'Enter'){
+      searchQuery();
+    }
+  }
+
   return (
-    <div className="App">React App!</div>
+    <div className="App">
+      <h1>Hello World!!</h1>
+      <div className="form">
+        <input type="text" value={userQuery} onKeyPress={keyPressHandler} onChange={updateUserQuery} />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+    </div>
   );
 }
 
